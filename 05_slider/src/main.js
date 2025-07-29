099369013b95ba1fed3c85d24309b7230c44d46d
Emiliano Perez <emilianoperezpaponi@gmail.com>
2025-07-30 00:16:06 +0200

    Edit 05_Slider styles

--- 05_slider/src/main.js
+++ 05_slider/src/main.js
@@ -1,9 +1,12 @@
 import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
+import './style.css';
 
 const swiper = new Swiper(".miPrimerSlider", {
+  modules: [Navigation, Pagination],
   pagination: {
     el: ".swiper-pagination",
     type: "progressbar",
