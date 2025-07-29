268eb4d2dce53d09f0526f12a5a3006669915497
Emiliano Perez <emilianoperezpaponi@gmail.com>
2025-07-29 23:54:50 +0200

    add 05_Slider Project

---
+++ 05_slider/src/main.js
@@ -0,0 +1,15 @@
+import Swiper from 'swiper';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+const swiper = new Swiper(".miPrimerSlider", {
+  pagination: {
+    el: ".swiper-pagination",
+    type: "progressbar",
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
