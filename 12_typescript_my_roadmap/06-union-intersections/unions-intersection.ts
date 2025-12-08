06922941346255626b1b878c245604756d79b8c8
Emiliano Perez <emilianoperezpaponi@gmail.com>
2025-12-08 21:26:19 +0100

    add union-intersection for typescript

---
+++ 12_typescript_my_roadmap/06-union-intersections/unions-intersection.ts
@@ -0,0 +1,46 @@
+// Ejercicio 1
+type On = { estado: "Encendida" };
+type Off = { estado: "Apagada" };
+
+type EstadoLuz = On | Off;
+
+function presionarBoton(e: EstadoLuz) {
+  if (e.estado === "Encendida") {
+    console.log(`Encendiendo...`)
+  } else {
+    console.log(`La luz está apagada`)
+  }
+}
+presionarBoton({ estado: "Encendida" });
+presionarBoton({ estado: "Apagada" });
+
+// Ejercicio 2
+type Persona = { nombre: string };
+type Empleado = { salario: number };
+
+type Trabajador = Persona & Empleado;
+
+function info(t: Trabajador) {
+  console.log(`Nombre del empleado ${t.nombre} Salario ${t.salario}`);
+}
+// Creo un ejemplo de Trabajador
+const dev1: Trabajador = {
+  nombre: "Emiliano",
+  salario: 2500,
+};
+
+info(dev1);
+
+// Ejercicio 3
+type Resultado = number | string;
+
+function procesar(r: Resultado) {
+  if (typeof r === "string") {
+    console.log("Texto procesado")
+  } else {
+    console.log("Número procesado")
+  }
+};
+
+procesar("Hoola");
+procesar(5);
